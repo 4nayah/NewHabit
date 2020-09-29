505763fbc1645a37995b8cf2b1aab6faa23846c8
4nayah <46417724+4nayah@users.noreply.github.com>
2020-09-29 18:25:05 +0200

    Gh.io warmup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
-
 import reducer from "./store/reducer";
 import { Provider } from "react-redux";
-
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -13,7 +12,9 @@ const store = createStore(reducer);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   rootElement
